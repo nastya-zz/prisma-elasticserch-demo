36f2dbb1778bbab67caeabdc40d7df54f2e58dfd
nastya <anastasia.zelenenkaya@aisa.ru>
2022-09-17 19:50:59 +0300

    update post dto

--- src/post/dto/create-post.dto.ts
+++ src/post/dto/create-post.dto.ts
@@ -1,15 +1,9 @@
-import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
-
 export class CreatePostDto {
-  @IsString()
-  @IsNotEmpty()
   title: string;
 
-  @IsString()
-  @IsNotEmpty()
   content: string;
 
-  @IsNotEmpty()
-  @IsNumber()
-  userId: number;
+  show: boolean;
+
+  authorId: number;
 }
